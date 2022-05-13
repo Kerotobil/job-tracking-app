17071afc8c791c077adabaf15e808b43976a7173
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-13 15:23:38 +0300

    Add redux-dev-toosl extension on Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app'
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from '../store';
 import { Provider } from "react-redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 
 function MyApp({ Component, pageProps }: AppProps) {
