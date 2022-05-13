469338402190244ff7e5a105d81df8de2deaae23
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-13 15:23:15 +0300

    Add form and methods on createJob component

--- components/createJob/index.tsx
+++ components/createJob/index.tsx
@@ -1,49 +1,65 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Button, FormControl, MenuItem, Select, TextField } from "@mui/material";
+import { Form, Formik } from "formik";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addJob } from "../../store/actions/addJob";
+import { Priority } from "../../store/types/job";
 import styles from "./createJob.module.scss"
 
 export const CreateJob = () => {
 
-    interface Priority {
-        type: "urgent" | "regularly" | "trivial"
-        sira: number,
-        text: string
-    }
+    const dispatch = useDispatch();
 
-    const priority: Priority[] = [
-        { type: "urgent", sira: 0, text: "Acil" },
-        { type: "regularly", sira: 1, text: "Normal" },
-        { type: "trivial", sira: 2, text: "Önemsiz" }
-    ]
-
-    const [select, setSelect] = useState<Priority>();
-
-    const handleSelect = (select: Priority) => {
-
-        setSelect(select)
-    }
+    const [form, setForm] = useState<{ jobName: string, priority: Priority }>({
+        jobName: "", priority: "Regularly"
+    });
+    const priority: { type: Priority, text: string }[] = [
+        { type: "Urgent", text: "Acil" },
+        { type: "Regularly", text: "Normal" },
+        { type: "Trivial", text: "Önemsiz" }
+    ];
 
     return (
-        <div className={styles.container}>
-            <div className={styles.jobNameContainer}>
-                <div><label>{"İş Adı"}</label></div>
-                <TextField className={styles.jobName} size="small" placeholder="Ne İşin Var?" variant="outlined" />
-            </div>
-            <div className={styles.priorityContainer}>
-                <div>
-                    <label>{"Öncelik"}</label>
-                </div>
-                <FormControl className={styles.selectPriority} size="small">
-                    <Select
-                        onChange={(e) => console.log(e)}>
-                        {priority.map((item, index) => (<MenuItem key={index} value={item.type}>{item.text}</MenuItem>))
-                        }
-                    </Select>
-                </FormControl>
-            </div>
-            <div className={styles.ButtonContainer}>
-                <div><label>{""}</label></div>
-                <Button className={styles.createButton} variant="contained" >Oluştur</Button>
-            </div>
-        </div>)
+        <Formik
+            initialValues={form}
+            onSubmit={(values) => {
+                console.log(values)
+                dispatch(addJob({ item: { type: "ADD_JOB", payload: { text: values.jobName, priority: values.priority } } }))
+            }} >
+            {(formikProps) => (
+                <Form>
+                    <div className={styles.container}>
+                        <div className={styles.jobNameContainer}>
+                            <div><label>{"İş Adı"}</label></div>
+                            <TextField
+                                name="jobName"
+                                value={formikProps.values.jobName}
+                                onChange={formikProps.handleChange}
+                                className={styles.jobName}
+                                size="small"
+                                placeholder="Ne İşin Var?"
+                                variant="outlined" />
+                        </div>
+                        <div className={styles.priorityContainer}>
+                            <div>
+                                <label>{"Öncelik"}</label>
+                            </div>
+                            <FormControl className={styles.selectPriority} size="small">
+                                <Select
+                                    name="priority"
+                                    value={formikProps.values.priority}
+                                    onChange={(e) => formikProps.handleChange(e)}>
+                                    {priority.map((item, index) => (<MenuItem key={index} value={item.type}>{item.text}</MenuItem>))
+                                    }
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <div className={styles.ButtonContainer}>
+                            <div><label>{""}</label></div>
+                            <Button type="submit" className={styles.createButton} variant="contained" >Oluştur</Button>
+                        </div>
+                    </div>
+                </Form>)}
+        </Formik>
+    )
 }
