e57ed010b36c7b5b8ffc4543baac8510aad330fe
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-13 14:03:26 +0300

    Add Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app'
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from '../store';
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp
