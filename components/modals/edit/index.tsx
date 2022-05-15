1d29301eb6bc57b7017b1fe504e1c181d32063e5
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-15 13:04:32 +0300

    Add event props editModal component and set titles

--- components/modals/edit/index.tsx
+++ components/modals/edit/index.tsx
@@ -2,18 +2,15 @@ import { Button, FormControl, MenuItem, Select, TextField } from '@mui/material'
 import { Form, Formik } from 'formik';
 import { Job, Priority } from '../../../store/types/job';
 import styles from './edit.module.scss';
-
 import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { addJob } from '../../../store/actions/addJob';
+import { useState } from 'react';
 interface Props {
   job: Job;
-  saveEvent: () => void;
+  saveEvent: (e: Priority) => void;
   cancelEvent: () => void;
 }
 
 export const EditModal = (props: Props) => {
-  const dispatch = useDispatch();
   const [form, setForm] = useState<{ jobName: string; priority: Priority }>({
     jobName: props.job?.text,
     priority: props.job?.priority,
@@ -34,7 +31,7 @@ export const EditModal = (props: Props) => {
       <Formik
         initialValues={form}
         onSubmit={(values) => {
-          //  dispatch(addJob({ item: { type: 'ADD_JOB', payload: { text: values.jobName, priority: values.priority } } }));
+          props.saveEvent(values.priority);
         }}
       >
         {(formikProps) => (
@@ -42,7 +39,7 @@ export const EditModal = (props: Props) => {
             <div className={styles.container}>
               <div className={styles.jobNameContainer}>
                 <div>
-                  <label>{'İş Adı'}</label>
+                  <label>{'Job Name'}</label>
                 </div>
                 <TextField
                   name="jobName"
@@ -51,13 +48,12 @@ export const EditModal = (props: Props) => {
                   onChange={formikProps.handleChange}
                   className={styles.jobName}
                   size="small"
-                  placeholder="Ne İşin Var?"
                   variant="outlined"
                 />
               </div>
               <div className={styles.priorityContainer}>
                 <div>
-                  <label>{'Öncelik'}</label>
+                  <label>{'Priority'}</label>
                 </div>
                 <FormControl className={styles.selectPriority} size="small">
                   <Select
