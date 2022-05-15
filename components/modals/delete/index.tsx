3211d2dec5c04611126347af950256c199deec1b
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-15 13:16:29 +0300

    Edit deleteModal

--- components/modals/delete/index.tsx
+++ components/modals/delete/index.tsx
@@ -1,8 +1,7 @@
-import { Button, FormControl, MenuItem, Select, TextField } from '@mui/material';
-import { Form, Formik } from 'formik';
-import { Job, Priority } from '../../../store/types/job';
-import styles from './edit.module.scss';
-import { useState } from 'react';
+import { Button } from '@mui/material';
+import { Job } from '../../../store/types/job';
+import styles from './delete.module.scss';
+
 interface Props {
   job: Job;
   saveEvent: () => void;
@@ -14,15 +13,17 @@ export const DeleteModal = (props: Props) => {
     <div className={styles.container}>
       <div>
         <div>
-          <h1>Are you sure you want to delete it? </h1>
+          <h1 className={styles.modalQuestion}>Are you sure you want to delete it? </h1>
         </div>
       </div>
-      <Button onClick={() => props.cancelEvent()} className={styles.createButton} variant="contained">
-        No
-      </Button>
-      <Button onClick={() => props.saveEvent()} className={styles.createButton} variant="contained">
-        Yes
-      </Button>
+      <div className={styles.ButtonContainer}>
+        <Button onClick={() => props.cancelEvent()} className={styles.createButton} variant="outlined" color="error">
+          No
+        </Button>
+        <Button onClick={() => props.saveEvent()} className={styles.createButton} variant="contained" color="error">
+          Delete
+        </Button>
+      </div>
     </div>
   );
 };
