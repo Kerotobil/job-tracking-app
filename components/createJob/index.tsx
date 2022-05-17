b8dc61548d6689b7ca350322de68546ce2826bc6
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-17 22:11:43 +0300

    Add text validation on createJob form

--- components/createJob/index.tsx
+++ components/createJob/index.tsx
@@ -1,33 +1,50 @@
-import { Button, FormControl, MenuItem, Select, TextField } from '@mui/material';
-import { Form, Formik } from 'formik';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addJob } from '../../store/actions/addJob';
-import { Priority } from '../../store/types/job';
-import styles from './createJob.module.scss';
-import * as yup from 'yup';
+import {
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addJob } from "../../store/actions/addJob";
+import { Priority } from "../../store/types/job";
+import styles from "./createJob.module.scss";
+import * as yup from "yup";
 
 export const CreateJob = () => {
   const dispatch = useDispatch();
 
   const [form, setForm] = useState<{ jobName: string; priority: Priority }>({
-    jobName: '',
-    priority: 'Regularly',
+    jobName: "",
+    priority: "Regularly",
   });
   const priority: { type: Priority; text: string }[] = [
-    { type: 'Urgent', text: 'Urgent' },
-    { type: 'Regularly', text: 'Regularly' },
-    { type: 'Important', text: 'Important' },
+    { type: "Urgent", text: "Urgent" },
+    { type: "Regularly", text: "Regularly" },
+    { type: "Important", text: "Important" },
   ];
 
   return (
     <Formik
       initialValues={form}
       validationSchema={yup.object().shape({
-        jobName: yup.string().required('Required').max(255),
+        jobName: yup
+          .string()
+          .matches(/^[A-Za-z-ğüşöçıİĞÜŞÖÇ 0-9]*$/, "Only letters and numbers")
+          .required("Required")
+          .max(255, "Max 255 characters"),
       })}
       onSubmit={(values) => {
-        dispatch(addJob({ item: { type: 'ADD_JOB', payload: { text: values.jobName, priority: values.priority } } }));
+        dispatch(
+          addJob({
+            item: {
+              type: "ADD_JOB",
+              payload: { text: values.jobName, priority: values.priority },
+            },
+          })
+        );
       }}
     >
       {(formikProps) => (
@@ -35,7 +52,7 @@ export const CreateJob = () => {
           <div className={styles.container}>
             <div className={styles.jobNameContainer}>
               <div>
-                <label>{'Job Name'}</label>
+                <label>{"Job Name"}</label>
               </div>
               <TextField
                 name="jobName"
@@ -45,11 +62,15 @@ export const CreateJob = () => {
                 size="small"
                 placeholder="What are you going to do?"
                 variant="outlined"
+                color={formikProps.errors.jobName ? "error" : undefined}
               />
+              {formikProps.errors.jobName && (
+                <p style={{ color: "red" }}>{formikProps.errors.jobName} </p>
+              )}
             </div>
             <div className={styles.priorityContainer}>
               <div>
-                <label>{'Priority'}</label>
+                <label>{"Priority"}</label>
               </div>
               <FormControl className={styles.selectPriority} size="small">
                 <Select
@@ -67,9 +88,13 @@ export const CreateJob = () => {
             </div>
             <div className={styles.ButtonContainer}>
               <div>
-                <label>{''}</label>
+                <label>{""}</label>
               </div>
-              <Button type="submit" className={styles.createButton} variant="contained">
+              <Button
+                type="submit"
+                className={styles.createButton}
+                variant="contained"
+              >
                 Create
               </Button>
             </div>
