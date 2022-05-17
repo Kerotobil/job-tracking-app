5ca872cde5d3772173ff8d6b800a0575a6b9a554
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-05-17 17:52:00 +0300

    Edit unnecessary import lines in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { store, persistor } from '../store';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
 //const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
